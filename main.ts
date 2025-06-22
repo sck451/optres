230dff7210788754465a4b6549872be9af4a9e7f
Sam Korn <sck451@gmail.com>
2025-06-22 09:02:49 +0100

    Add exports to main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,5 @@
 export { None, none, type Option, Some, some } from "./src/Option/Option.ts";
 export { Err, err, Ok, ok, type Result } from "./src/Result/Result.ts";
 export { UnwrapError } from "./src/UnwrapError/UnwrapError.ts";
+export { AsyncResult } from "./src/AsyncResult/AsyncResult.ts";
+export { AsyncOption } from "./src/AsyncOption/AsyncOption.ts";
